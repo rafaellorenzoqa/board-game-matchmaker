0407292a2e33d00298db2cdda25a684bac459734
Rafael <rafaellorenzo.cc@gmail.com>
2026-08-04 23:39:49 -0300

    Refactoring gameStore.js to receive a fixture as path

--- src/services/gameStore.js
+++ src/services/gameStore.js
@@ -3,52 +3,52 @@ const { readJsonFile, writeJsonFile } = require('../utils/jsonFile');
 
 const GAMES_FILE = path.join(__dirname, '..', '..', 'data', 'games.json');
 
-function listGames() {
-  return readJsonFile(GAMES_FILE);
+function listGames(filePath = GAMES_FILE) {
+  return readJsonFile(filePath);
 }
 
-function addGame(game) {
-  const games = listGames();
+function addGame(game, filePath = GAMES_FILE) {
+  const games = listGames(filePath);
   const nextId = games.length > 0 ? Math.max(...games.map((g) => g.id)) + 1 : 1;
   const newGame = { id: nextId, ...game };
 
   games.push(newGame);
-  writeJsonFile(GAMES_FILE, games);
+  writeJsonFile(filePath, games);
 
   return newGame;
 }
 
-function replaceGame(id, game) {
-  const games = listGames();
+function replaceGame(id, game, filePath = GAMES_FILE) {
+  const games = listGames(filePath);
   const index = games.findIndex((g) => g.id === id);
   if (index === -1) return null;
 
   const updatedGame = { id, ...game };
   games[index] = updatedGame;
-  writeJsonFile(GAMES_FILE, games);
+  writeJsonFile(filePath, games);
 
   return updatedGame;
 }
 
-function patchGame(id, updates) {
-  const games = listGames();
+function patchGame(id, updates, filePath = GAMES_FILE) {
+  const games = listGames(filePath);
   const index = games.findIndex((g) => g.id === id);
   if (index === -1) return null;
 
   const updatedGame = { ...games[index], ...updates };
   games[index] = updatedGame;
-  writeJsonFile(GAMES_FILE, games);
+  writeJsonFile(filePath, games);
 
   return updatedGame;
 }
 
-function deleteGame(id) {
-  const games = listGames();
+function deleteGame(id, filePath = GAMES_FILE) {
+  const games = listGames(filePath);
   const index = games.findIndex((g) => g.id === id);
   if (index === -1) return false;
 
   games.splice(index, 1);
-  writeJsonFile(GAMES_FILE, games);
+  writeJsonFile(filePath, games);
 
   return true;
 }
